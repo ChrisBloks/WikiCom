60099ae5e7581be5c24f286c0e7901b3e5c084d4
Christian Bloks <christian.bloks@gmail.com>
2026-08-24 14:41:31 +0200

    Modified maincontroller and moved getRequest

--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -3,9 +3,7 @@ require_once "./src/controllers/PageController.php";
 
 /**
  * Controller class for initial request handling and forwarding request to the correct subcontroller.
- * @var bool $posted true if MainController is accessed through a POST request, otherwise false.
  * @var bool $async true if MainController is accessed through an AJAX request, otherwise false.
- * @var string $action determines which (sub)controller should resolve the request.
  */
 class MainController
 {
@@ -15,17 +13,10 @@ class MainController
 
     /**
      * __construct
-     * @return void
      */
     public function __construct()
     {
-        $this->posted = ($_SERVER['REQUEST_METHOD'] === 'POST');
         $this->async = isset($_SERVER['HTTP_X_REQUESTED_WITH']);
-        $this->action = Utils::getRequestVar(
-            key: 'action',
-            frompost: $this->posted,
-            default: $this->posted ? '' : 'home'
-        );
     }
 
     /**
@@ -37,7 +28,6 @@ class MainController
         $this->getHandler()->handleRequest();
     }
 
-
     /**
      * Get the relevant subcontroller based on the request. Default is PageController.
      * @return iController
@@ -46,19 +36,11 @@ class MainController
     {   
         // Ajax request. Action is equal to a specific update
         if($this->async){
-            switch ($this->action) {
-                        case 'blah':
-                            // return new AjaxController($this->_crud);
-                            // break;
-                        default:
-                            // TODO: overwrite
-                            return new PageController($this->posted, $this->action);
-                    }
+            // return new AjaxController();
         }
+
+        // Default
         // Non-ajax request. Action is equal to the requested page
-        else {
-            return new PageController($this->posted, $this->action);
-        }
-        
+        return new PageController();
     }
 }
