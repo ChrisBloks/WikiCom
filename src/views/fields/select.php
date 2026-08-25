f87dfc50e0f06e881226350668143628725112fe
Christian Bloks <christian.bloks@gmail.com>
2026-08-25 15:31:51 +0200

    Yes!

--- src/views/fields/select.php
+++ src/views/fields/select.php
@@ -1,28 +1,31 @@
 <?php
+
 namespace Wiki\views\fields;
 
 use Wiki\tools\utils\HtmlUtils;
 
-class Select extends BaseField{
+class Select extends BaseField
+{
 
-protected array $options =[];
+    protected array $options = [];
 
-    public function __construct(string $name, string $label, string $class, array $options){
-        parent::__construct($name, $label, $class);    
+    public function __construct(string $name, string $label, string $class, array $options)
+    {
+        parent::__construct($name, $label, $class);
         $this->options = $options;
-        
     }
 
 
-    public function show():string{
-        
-        $ret = HtmlUtils::printLabel($this->id, $this->label)
-             . '<select'. $this->baseAttribs(). ">";
+    public function show(): string
+    {
 
-        foreach ($this->options as $value=>$display){
-            $ret .= '<option value="'.$value.'">'. $display.'</option>';
+        $ret = HtmlUtils::printLabel($this->id, $this->label)
+            . '<select' . $this->baseAttribs() . ">";
+
+        foreach ($this->options as $value => $display) {
+            $ret .= '<option value="' . $value . '">' . $display . '</option>';
         }
-                    
+
         return $ret .= "</select><br>";
     }
 }
