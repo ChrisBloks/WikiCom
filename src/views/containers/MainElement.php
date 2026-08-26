0eee532b5d582026b661fb97752a379e400fb741
MariusJ98 <mariusjoskin@gmail.com>
2026-08-26 12:03:43 +0200

    Addition of containers for page elements

---
+++ src/views/containers/MainElement.php
@@ -0,0 +1,10 @@
+<?php
+namespace Wiki\views\containers;
+
+class MainElement extends ContainerElement{
+
+
+    public function __construct(){
+        parent::__construct('<main>','</main>');
+    }
+}
