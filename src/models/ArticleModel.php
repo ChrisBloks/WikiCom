3abaf4249865ce1469927ff7be857baf2698370d
Christian Bloks <christian.bloks@gmail.com>
2026-08-27 11:18:56 +0200

    Restructured and documented ArticleModel and Crud

--- src/models/ArticleModel.php
+++ src/models/ArticleModel.php
@@ -21,10 +21,10 @@ class ArticleModel extends BaseModel
      * @var array
      */
     private static $sort_values = [
-            "rating" => "AVGrating",
-            "AVGrating" => "AVGrating",
-            "datum" => "article.lastEdit",
-            "date" => "article.lastEdit"
+        "rating" => "AVGrating",
+        "AVGrating" => "AVGrating",
+        "datum" => "article.lastEdit",
+        "date" => "article.lastEdit"
     ];
 
 
@@ -35,7 +35,7 @@ class ArticleModel extends BaseModel
      */
     public function fetchArticleById(int $article_id): array|false
     {
-            $sql = "SELECT  article.title,
+        $sql = "SELECT  article.title,
                             user.name,
                             article.summary,
                             article.codeBlock,
@@ -44,12 +44,12 @@ class ArticleModel extends BaseModel
                     FROM wiki_article as article
                     JOIN user ON article.user_id=user.id 
                     WHERE article.id=:article_id";
-            $params = ['article_id' => $article_id];
-            $result = $this->crudTemp->selectOne($sql, $params);
-            if (empty($result)) {
-                    $result = [];
-            }
-            return $result;
+        $params = ['article_id' => $article_id];
+        $result = $this->crudTemp->selectOne(sql: $sql, params: $params);
+        if ($result === false) {
+            $result = [];
+        }
+        return $result;
     }
 
     /**
@@ -59,17 +59,17 @@ class ArticleModel extends BaseModel
      */
     public function fetchArticleByUserId(int $user_id): array|false
     {
-            $sql = "SELECT  article.id,
+        $sql = "SELECT  article.id,
                             article.title, 
                             article.lastEdit 
                     FROM wiki_article as article
                     WHERE user_id=:user_id";
-            $params = ['user_id' => $user_id];
-            $result = $this->crudTemp->selectMany($sql, $params);
-            if (empty($result)) {
-                    $result = false;
-            }
-            return $result;
+        $params = ['user_id' => $user_id];
+        $result = $this->crudTemp->selectMany(sql: $sql, params: $params);
+        if (empty($result)) {
+            $result = false;
+        }
+        return $result;
     }
 
     /**
@@ -86,6 +86,7 @@ class ArticleModel extends BaseModel
         // Check if sortBy is a valid sorting method
         $sortBy = array_key_exists($sortBy, self::$sort_values) ? self::$sort_values[$sortBy] : 'article.lastEdit';
 
+
         $base_select_clause = "SELECT DISTINCT 
                                 article.id,
                                 article.title, 
@@ -93,51 +94,52 @@ class ArticleModel extends BaseModel
                                 article.lastEdit";
 
         $extra_select_clause = ", COALESCE(vr.AVGrating, 0) AS rating" .
-                                (!empty($author_ids) ? // If authors are specified
-                                    " ,user.name as author" : // get the authors
-                                    ""); // get average rating
+            (!empty($author_ids) ? // If authors are specified
+                " ,user.name as author" : // get the authors
+                ""); // get average rating
 
         $from_clause = " FROM wiki_article as article";
 
+        // Building the JOIN and WHERE clauses
         // For storing mapping of placeholder to variables for PREPARED statement
         $params = [];
-        [$join_clause, $where_clause] = (function($author_ids, $tag_ids, &$params){
-                // Iteratively add neccessary parts to the join and where clauses
-                $join_clause = " JOIN v_article_avg_rating as vr ON vr.id = article.id";
-                $where_statements = [];
-                
-                // check if any tags are given and build IN clause
-                if (!empty($tag_ids)){
-                    $join_clause .=  ' JOIN wiki_article_to_tag att ON att.article_id = article.id' .
-                                        ' JOIN wiki_tag ON wiki_tag.id = att.wiki_tag_id';
-                    [$in_clause, $placeholder_mapping] = $this->buildInClause('att.wiki_tag_id', $tag_ids, 'tag');
-                    $where_statements[] = $in_clause;
-                    $params = array_merge($params, $placeholder_mapping);
-                }
-                // check if any authors are given and build IN clause
-                if (!empty($author_ids)){
-                    $join_clause .= ' JOIN user ON user.id = article.user_id ';
-                    [$in_clause, $placeholder_mapping] = $this->buildInClause('article.user_id', $author_ids, 'user');
-                    $where_statements[] = $in_clause;
-                    $params = array_merge($params, $placeholder_mapping);
-                }
+        [$join_clause, $where_clause] = (function ($author_ids, $tag_ids, &$params) {
+            // Iteratively add neccessary parts to the join and where clauses
+            $join_clause = " JOIN v_article_avg_rating as vr ON vr.id = article.id";
+            $where_statements = [];
 
-                // If where statements are added, return the where clause, otherwise return nothing
-                $where_clause = (!empty($where_statements) ? ' WHERE ' . implode(" AND ", $where_statements) : '');
-                return [$join_clause, $where_clause];
+            // check if any tags are given and build IN clause
+            if (!empty($tag_ids)) {
+                $join_clause .=  ' JOIN wiki_article_to_tag att ON att.article_id = article.id' .
+                    ' JOIN wiki_tag ON wiki_tag.id = att.wiki_tag_id';
+                [$in_clause, $placeholder_mapping] = $this->buildInClause(reference: 'att.wiki_tag_id', values: $tag_ids, prefix: 'tag');
+                $where_statements[] = $in_clause;
+                $params = array_merge($params, $placeholder_mapping);
+            }
+            // check if any authors are given and build IN clause
+            if (!empty($author_ids)) {
+                $join_clause .= ' JOIN user ON user.id = article.user_id ';
+                [$in_clause, $placeholder_mapping] = $this->buildInClause(reference: 'article.user_id', values: $author_ids, prefix: 'user');
+                $where_statements[] = $in_clause;
+                $params = array_merge($params, $placeholder_mapping);
+            }
+
+            // If where statements are added, return the where clause, otherwise return nothing
+            $where_clause = (!empty($where_statements) ? ' WHERE ' . implode(" AND ", $where_statements) : '');
+            return [$join_clause, $where_clause];
         })($author_ids, $tag_ids, $params);
 
         $order_by_clause = ' ORDER BY ' . $sortBy . ';';
 
         // Connect the clauses
         $sql =  $base_select_clause .
-                $extra_select_clause .
-                $from_clause . 
-                $join_clause . 
-                $where_clause . 
-                $order_by_clause;
+            $extra_select_clause .
+            $from_clause .
+            $join_clause .
+            $where_clause .
+            $order_by_clause;
 
-        return $this->crudTemp->selectMany($sql, $params);
+        return $this->crudTemp->selectMany(sql: $sql, params: $params);
     }
 
     /**
@@ -157,57 +159,64 @@ class ArticleModel extends BaseModel
      */
     private function buildInClause(string $reference, array $values, string $prefix): array
     {
-            $placeholders = []; // Will contain strings for binding variables
-            $params = []; // Will contain a mapping of placeholders to variables
+        $placeholders = []; // Will contain strings for binding variables
+        $params = []; // Will contain a mapping of placeholders to variables
 
-            // Add each value to placeholders and params with correct formatting
-            foreach (array_values($values) as $i => $value) {
-                    $key = $prefix . '_' . $i;
-                    $placeholders[] = ':' . $key;
-                    $params[$key] = $value;
-            }
+        // Add each value to placeholders and params with correct formatting
+        foreach (array_values($values) as $i => $value) {
+            $key = $prefix . '_' . $i;
+            $placeholders[] = ':' . $key;
+            $params[$key] = $value;
+        }
 
-            // Generate IN clause 
-            $result = $reference . ' IN (' . implode(',', $placeholders) . ')';
+        // Generate IN clause 
+        $result = $reference . ' IN (' . implode(',', $placeholders) . ')';
 
-            return [$result, $params];
+        return [$result, $params];
     }
 
 
-    /*
-    * Method that saves article to the database given the article info
-    *
-    * @params article info + user id
-    */
-    public function saveNewArticleInfo($article_title, $article_summary, $article_codeBlock, $imgFileName, $user_id)
+    /**
+     * Saves new article to the database given the article info
+     * @param string $article_title
+     * @param string $article_summary
+     * @param string $article_codeBlock
+     * @param string $imgFileName
+     * @param int $user_id id of the author.
+     * @return int|false
+     */
+    public function saveNewArticleInfo(string $article_title, string $article_summary, string $article_codeBlock, string $imgFileName, int $user_id): int|false
     {
-            $sql = "INSERT INTO article (title, summary, codeBlock, imgFileName, user_id, lastEdit)
+        $sql = "INSERT INTO wiki_article (title, summary, codeBlock, imgFileName, user_id, lastEdit)
                     VALUES (:title,:summary,:codeBlock,:imgFileName,:user_id,:lastEdit)";
-            $params = [
-                    ':title' => $article_title,
-                    ':summary' => $article_summary,
-                    ':codeBlock' => $article_codeBlock,
-                    ':imgFileName' => $imgFileName,
-                    ':user_id' => $user_id,
-                    ':lastEdit' => date('Y-m-d'),
-            ];
-            $result = $this->crudTemp->doInsert($sql, $params);
-            if (empty($result)) {
-                    $this->logError("Saving article didn't work idk");
-                    $result = false;
-            }
-            return $result;
+        $params = [
+            ':title' => $article_title,
+            ':summary' => $article_summary,
+            ':codeBlock' => $article_codeBlock,
+            ':imgFileName' => $imgFileName,
+            ':user_id' => $user_id,
+            ':lastEdit' => date(format: 'Y-m-d'),
+        ];
+        $result = $this->crudTemp->doInsert(sql: $sql, params: $params);
+        if ($result === false) {
+            $this->logError(message: "Failed to save article to the database.");
+        }
+        return $result;
     }
 
-    /*
-    * Method that updates article based on new information
-    *
-    * @params article info + user id
-    */
-
-    public function saveExistingArticleInfo($article_id, $article_title, $article_summary, $article_codeBlock, $imgFileName, $user_id)
+    /**
+     * Updates an existing article
+     * @param int $article_id 
+     * @param string $article_title
+     * @param string $article_summary
+     * @param string $article_codeBlock
+     * @param string $imgFileName
+     * @param int $user_id id of the author
+     * @return bool
+     */
+    public function saveExistingArticleInfo(int $article_id, string $article_title, string $article_summary, string $article_codeBlock, string $imgFileName, int $user_id): int|false
     {
-            $sql = "UPDATE  article
+        $sql = "UPDATE  article
                     SET     title = :title,
                             summary = :summary,
                             codeBlock = :codeBlock,
@@ -215,90 +224,73 @@ class ArticleModel extends BaseModel
                             user_id = :user_id,
                             lastEdit = :lastEdit
                     WHERE   id = :id";
-            $params = [
-                    ":id" => $article_id,
-                    ':title' => $article_title,
-                    ':summary' => $article_summary,
-                    ':codeBlock' => $article_codeBlock,
-                    ':imgFileName' => $imgFileName,
-                    ':user_id' => $user_id,
-                    ':lastEdit' => date('Y-m-d'),
-            ];
+        $params = [
+            ":id" => $article_id,
+            ':title' => $article_title,
+            ':summary' => $article_summary,
+            ':codeBlock' => $article_codeBlock,
+            ':imgFileName' => $imgFileName,
+            ':user_id' => $user_id,
+            ':lastEdit' => date('Y-m-d'),
+        ];
 
-            try {
-                    $this->crudTemp->prepareAndExecute($sql, $params);
-                    return true;
-            } catch (\PDOException $e) {
-                    // $this->logError($e->getMessage());
-                    return false;
-            }
+        return $this->crudTemp->doUpdate(sql: $sql, params: $params);
     }
 
-    /*
-    * Method that checks if tag already exists. If it does not exist give true
-    *
-    * @param tag name
-    */
 
-    public function checkTag($tag_name)
+    /**
+     * Checks if a tag already exists.
+     * @param string $tag_name
+     * @return bool True if $tag_name matches an entry in the database, false otherwise.
+     */
+    public function checkTagExists(string $tag_name): bool
     {
-            $sql = "SELECT name FROM tag 
+        $sql = "SELECT name FROM tag 
                     WHERE name=:tag_name";
-            $params = ["tag_name" => $tag_name];
-            $result = $this->crudTemp->selectOne($sql, $params);
-            return empty($result);
+        $params = ["tag_name" => $tag_name];
+        $result = $this->crudTemp->selectOne(sql: $sql, params: $params);
+        return !empty($result);
     }
 
-    /*
-    * Method that checks if title already exists. If it does not exist give true
-    *
-    * @param title name
-    */
-
-    public function checkTitle($title_name)
+    /**
+     * Checks if a title already exists.
+     * @param string $title_name
+     * @return bool True if $title_name matches an entry in the database, false otherwise.
+     */
+    public function checkTitleExists(string $title_name): bool
     {
-            $sql = "SELECT title FROM wiki_article 
+        $sql = "SELECT title FROM wiki_article 
                     WHERE title=:title_name";
-            $params = ["title_name" => $title_name];
-            $result = $this->crudTemp->selectOne($sql, $params);
-            return empty($result);
+        $params = ["title_name" => $title_name];
+        $result = $this->crudTemp->selectOne(sql: $sql, params: $params);
+        return empty($result);
     }
 
-    /*
-    * Method that adds a new tag to the database
-    *
-    * @param tag name
-    */
-    public function addNewTag(string $tag_name)
+    /**
+     * Inserts a new tag into the database
+     * @param string $tag_name
+     * @return int|false database id of new tag or false on failure
+     */
+    public function addNewTag(string $tag_name): int|false
     {
-            $sql = "INSERT INTO tag (name) 
+        $sql = "INSERT INTO tag (name) 
                     VALUES (:tag_name)";
-            $params = ["tag_name" => $tag_name];
-            try {
-                    $this->crudTemp->prepareAndExecute($sql, $params);
-                    return true;
-            } catch (\PDOException $e) {
-                    // $this->logError($e->getMessage());
-                    return false;
-            }
+        $params = ["tag_name" => $tag_name];
+        return $this->crudTemp->doInsert(sql: $sql, params: $params);
     }
 
-    /*
-    * Method that adds tag to article
-    *
-    * @params article id and tag id
-    */
-    public function addTagToArticle(int $article_id, int $tag_id)
+    /**
+     * Adds a bridge-table entry between a given article and tag
+     *
+     * @param int $article_id
+     * @param int $tag_id
+     * @return int|false New bridge-table row id or false on failure
+     */
+    public function addTagToArticle(int $article_id, int $tag_id): int|false
     {
-            $sql = "INSERT INTO article_to_tag (article_id, tag_id) 
+        $sql = "INSERT INTO article_to_tag (article_id, tag_id) 
                     VALUES (:article_id,:tag_id)";
-            $params = ["article_id" => $article_id, "tag_id" => $tag_id];
-            try {
-                    $this->crudTemp->prepareAndExecute($sql, $params);
-                    return true;
-            } catch (\PDOException $e) {
-                    // $this->logError($e->getMessage());
-                    return false;
-            }
+        $params = ["article_id" => $article_id, "tag_id" => $tag_id];
+        return $this->crudTemp->doInsert(sql: $sql, params: $params);
     }
 }
--- src/models/Crud.php
+++ src/models/Crud.php
@@ -7,11 +7,13 @@
 
 namespace Wiki\models;
 
+use Wiki\tools\traits\tErrorMessageCollector;
 use Wiki\tools\traits\tSingleton;
 
 class Crud
 {
     use tSingleton;
+    use tErrorMessageCollector;
 
     public \PDO $db;
 
@@ -30,43 +32,93 @@ class Crud
         return new \PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     }
 
-    public function isConnected()
+    /**
+     * Check if CRUD is connected to the database.
+     * @return bool True if a PDO object exists within this CRUD, otherwise false.
+     */
+    public function isConnected(): bool
     {
-        return is_object($this->db);
+        return is_object(value: $this->db);
     }
 
-    public function selectOne($sql, $params)
+
+    /**
+     * Select a single entry from the database.
+     * @param string $sql
+     * @param array $params
+     * @return array|false A single row matching the query or false on failure.
+     */
+    public function selectOne(string $sql, array $params): array|false
     {
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
-        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+        $stmt = $this->prepareAndExecute(sql: $sql, params: $params);
+        $result = $stmt->fetch(mode: \PDO::FETCH_ASSOC);
         return $result;
     }
 
-    public function selectMany($sql, $params, $fetch = \PDO::FETCH_ASSOC)
+    /**
+     * Select multiple entries from the database.
+     * @param string $sql
+     * @param array $params
+     * @param int $fetch_mode
+     * @return array|false All rows matching the query or false on failure.
+     */
+    public function selectMany(string $sql, array $params, int $fetch_mode = \PDO::FETCH_ASSOC): array|false
     {
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
-        $result = $stmt->fetchAll($fetch);
+        $stmt = $this->prepareAndExecute(sql: $sql, params: $params);
+        $result = $stmt->fetchAll(mode: $fetch_mode);
         return $result;
     }
 
-    public function doInsert($sql, $params)
+    /**
+     * Insert a single new entry to the database
+     * @param string $sql
+     * @param array $params
+     * @return int|false Last insert row or false on failure.
+     */
+    public function doInsert(string $sql, array $params): int|false
     {
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
+        $this->prepareAndExecute(sql: $sql, params: $params);
         return $this->db->lastInsertId();
     }
 
-    public function prepareAndExecute($sql, $params)
+
+    /**
+     * Deletes one or more entries from the database
+     * @param string $sql
+     * @param array $params
+     * @return array|false Number of affected rows or false on failure.
+     */
+    public function doDelete(string $sql, array $params): int|false
     {
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
+        $stmt = $this->prepareAndExecute(sql: $sql, params: $params);
+        return $stmt->rowCount();
     }
 
-    public function doDelete($sql, $params)
+    /**
+     * Updates one or more rows in the database
+     * @param string $sql
+     * @param array $params
+     * @return int|false Number of affected rows or false on failure.
+     */
+    public function doUpdate(string $sql, array $params): int|false{
+        $stmt = $this->prepareAndExecute(sql: $sql, params: $params);
+        return $stmt->rowCount();
+    }
+
+    /**
+     * Prepare an SQL statement and execute
+     * @param string $sql
+     * @param array $params
+     * @return \PDOStatement|false
+     */
+    public function prepareAndExecute(string $sql, array $params): \PDOStatement|false
     {
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
+        try {
+            $stmt = $this->db->prepare(query: $sql);
+            return $stmt->execute(params: $params);
+        } catch (\PDOException $e) {
+            $this->logError($e->getMessage());
+            return false;
+        }
     }
 }
