03e3b36b0b6e4ecea531d7588cb2416cd8d38ba3
Marius <mariusjoskin@gmail.com>
2026-09-03 12:40:36 +0200

    Created a noticemessage container to display user errors or system errors

--- src/views/containers/NoticeMessage.php
+++ src/views/containers/NoticeMessage.php
@@ -1,31 +1,54 @@
 <?php
+/**
+ * Creates a bar for display error messages
+ */
 namespace Wiki\views\containers;
 
-use Wiki\tools\utils\HtmlUtils;
-use Wiki\views\containers\WrappedText;
+use Wiki\tools\utils\HtmlUtils,
+    Wiki\tools\interfaces\iElement;
 
-
-class NoticeMessage extends WrappedText
+class NoticeMessage implements iElement
 {
+    private array $typeClassMap = [
+        'error'   => 'alert alert-danger',
+        'message' => 'alert alert-success',
+    ];
 
-    public string $class;
-
-    public function __construct($text, $wrapper, string $class)
+    public function show(): string
     {
-        $this->class = $class;
-        parent::__construct($this->showErrors(), '');
-        unset($_SESSION['errors']);
-    }
+        $notices = $this->collectNotices();
 
-
-    private function showErrors(): string
-    {
-        $str = '';
-        foreach ($_SESSION['errors'] as $value) {
-            $str .= '<p' . HtmlUtils::addClassAttr($this->class) . '>' . $value . '</p>';
+        if (empty($notices)) {
+            return '';
         }
+
+        $str = '';
+        foreach ($notices as $notice) {
+            $class = $this->typeClassMap[$notice['type']];
+            $str .= '<p' . HtmlUtils::addClassAttr($class) . '>'
+                . htmlspecialchars($notice['text'])
+                . '</p>';
+        }
+
+        unset($_SESSION['errors'], $_SESSION['messages']);
 
         return $str;
     }
 
+    private function collectNotices(): array
+    {
+        $notices = [];
+
+        foreach ($_SESSION['errors'] ?? [] as $key => $error) {
+            if (is_int($key)) {
+                $notices[] = ['type' => 'error', 'text' => $error];
+            }
+        }
+
+        foreach ($_SESSION['messages'] ?? [] as $message) {
+            $notices[] = ['type' => 'message', 'text' => $message];
+        }
+
+        return $notices;
+    }
 }
